6183d1497c81778766b1e0a7b42c1fb10d5a07d7
Senseyluiz <senseyluiz@gmail.com>
2021-03-14 11:42:24 -0300

    Primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import tabuleiro.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-
-		System.out.println("Hellow");
+		Position pos =  new Position(3, 5);
+		System.out.println(pos);
+		
 
 	}
 
---
+++ src/tabuleiro/Position.java
@@ -0,0 +1,37 @@
+package tabuleiro;
+
+public class Position {
+	
+	public Position(int linha, int coluna) {		
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	private int linha;
+	private int coluna;
+	
+	
+	
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+	
+	public int getColuna() {
+		return this.coluna;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+	
+	@Override
+	public String toString() {
+		return linha + ", " + coluna;
+	}
+
+	
+}
